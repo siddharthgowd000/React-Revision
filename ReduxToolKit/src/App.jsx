a89fe9dd15bde423b62774f25d5b6934a83a8ede
siddharthgowd000 <siddharthgowd000@gmail.com>
2025-03-22 10:46:05 +0530

    Redux Tool Kit Done

---
+++ ReduxToolKit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Redux Tool Kit</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
